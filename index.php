cb8ddfb2b9b3fde663de353ab834f4fe8ad6159a
Markus Avonius <mavonius@gmail.com>
2018-04-16 13:50:44 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>lomake</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://cdn.ckeditor.com/4.9.1/standard/ckeditor.js"></script>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">etusivu</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="lomake.php">Julkaise oma artikkeli</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control  mr-sm-2" type="search" placeholder="Hae artikkeleita..." aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <H1>Julkaise tekstisi muiden joukossa</H1>
+
+        </div>
+      </div>
+    </div>
+<br />
+    <div class="copyright">
+     <div class="container">
+         <div class="row text-center">
+         	 <p>Copyright © 2018 All rights reserved</p>
+         </div>
+ 	   </div>
+    </div>
+
+
+  </body>
+</html>
